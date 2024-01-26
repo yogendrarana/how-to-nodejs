febcd212cb0f103356cad95f384a83e37e074385
yogendrarana <yogendrarana.tech@gmail.com>
2024-01-26 22:55:35 +0545

    Added child process module

---
+++ modules/child_processes/child_processes.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process');
+const child = spawn('pwd');
+child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
